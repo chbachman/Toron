4b0372b8f7bbd5c727012c29ac1ea8c17f0ec622
chbachman <cbachman@bachmangroup.com>
2019-01-31 15:24:53 -0600

    Add Redis Auth

--- src/main/kotlin/com/chbachman/toron/jedis/Jedis.kt
+++ src/main/kotlin/com/chbachman/toron/jedis/Jedis.kt
@@ -6,7 +6,8 @@ import redis.clients.jedis.*
 import redis.clients.jedis.ScanParams.SCAN_POINTER_START_BINARY
 
 val star = "*".toByteArray()
-val pool = JedisPool(JedisPoolConfig(), "localhost")
+val redisPassword: String = System.getenv("REDIS_PASSWORD")
+val pool = JedisPool(JedisPoolConfig(), "localhost", 6379, Protocol.DEFAULT_TIMEOUT, redisPassword)
 
 fun Jedis.scan(params: ScanParams) =
     scan(SCAN_POINTER_START_BINARY, params)
